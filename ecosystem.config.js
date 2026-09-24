9b85a4c0574504fa22d57fd595a284429fd87683
Jhon hurtado <jhonse2001@gmail.com>
2026-09-24 11:46:51 -0500

    feat: add ecosystem configuration for hospital-intelligence-api

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'hospital-intelligence-api',
+      cwd: __dirname,
+      script: './dist/server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M',
+      time: true,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
